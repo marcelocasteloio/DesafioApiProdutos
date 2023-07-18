830422586ebebc362af659fc823b223f7e0dd531
OtavioVillasBoas <contato.otaviovbsc@gmail.com>
2023-07-18 12:21:36 -0300

    base: `project and solution`

---
+++ src/MarceloCastelo.Desafio.ApiProdutos.WebApi/Controllers/BackOfficeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace MarceloCastelo.Desafio.ApiProdutos.WebApi.Controllers;
+
+[ApiController]
+public class BackOfficeController : ControllerBase
+{
+    [HttpPost]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Route("/api/v1/backoffice/customer/import")]
+    public async Task<IActionResult> ImportCustomerAsync(
+        CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
